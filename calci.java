0dcf29f3eef842eb5045caabe36d25e8d11765ac
komaragiriMuraliPriya <121532826+komaragiriMuraliPriya@users.noreply.github.com>
2024-04-01 07:07:54 +0530

    Update calci.java

--- calci.java
+++ calci.java
@@ -5,14 +5,14 @@ public class BonusCalculator {
         System.out.print("Enter the current year: ");
         int currentYear = scanner.nextInt();
         System.out.print("Enter the year of joining: ");
-        int yearOfJoining = scanner.nextInt();
-        int yearsOfService = currentYear - yearOfJoining;
+        int joiningYear = scanner.nextInt();
+        int yearsOfService = currentYear - joiningYear;
         if (yearsOfService > 5) {
-            System.out.println("Congratulations! You have served for more than 5 years. You get a bonus of Rs. 5000/-");
+            System.out.println("Bonus: Rs. 5000/-");
         } else if (yearsOfService >= 3) {
-            System.out.println("Congratulations! You have served for 3 to 5 years. You get a bonus of Rs. 3000/-");
+            System.out.println("Bonus: Rs. 3000/-");
         } else {
-            System.out.println("Sorry, no bonus is awarded as you have served for less than 3 years.");
+            System.out.println("No bonus awarded.");
         }
         scanner.close();
     }
